43967e71f12ec357d2eba44dde5417aa0180eb1d
zvaraondrej <zvara.ondrej@gmail.com>
2017-08-03 10:09:03 +0200

    Extensible hint styles

--- src/PasswordField.js
+++ src/PasswordField.js
@@ -2,40 +2,38 @@
  * Notice: Some code was adapted from Material-UI's TextField component.
  *         Copyright (c) 2014 Call-Em-All (https://github.com/callemall/material-ui)
  */
-import React from 'react'
-import PropTypes from 'prop-types'
-import TextField from 'material-ui/TextField'
-import IconButton from 'material-ui/IconButton'
-import Visibility from 'material-ui/svg-icons/action/visibility'
-import VisibilityOff from 'material-ui/svg-icons/action/visibility-off'
-import transitions from 'material-ui/styles/transitions'
-import {fade} from 'material-ui/utils/colorManipulator'
+import React from "react";
+import PropTypes from "prop-types";
+import TextField from "material-ui/TextField";
+import IconButton from "material-ui/IconButton";
+import Visibility from "material-ui/svg-icons/action/visibility";
+import VisibilityOff from "material-ui/svg-icons/action/visibility-off";
+import transitions from "material-ui/styles/transitions";
+import { fade } from "material-ui/utils/colorManipulator";
 
 const getStyles = (props, context, state) => {
-  const {
-    textField: {
-      disabledTextColor,
-      errorColor
-    }
-  } = context.muiTheme
+  const { textField: { disabledTextColor, errorColor } } = context.muiTheme;
 
   const styles = {
     root: {
-      position: 'relative',
-      display: props.fullWidth ? 'block' : 'inline-block'
+      position: "relative",
+      display: props.fullWidth ? "block" : "inline-block"
     },
     input: {
       paddingRight: 59,
-      boxSizing: 'border-box'
+      boxSizing: "border-box"
     },
-    hint: {
-      position: 'relative',
-      bottom: 2,
-      fontSize: 12,
-      lineHeight: '12px',
-      color: props.errorText ? errorColor : fade(disabledTextColor, 0.5),
-      transition: transitions.easeOut()
-    },
+    hint: Object.assign(
+      {
+        position: "relative",
+        bottom: 2,
+        fontSize: 12,
+        lineHeight: "12px",
+        color: props.errorText ? errorColor : fade(disabledTextColor, 0.5),
+        transition: transitions.easeOut()
+      },
+      props.hintStyle
+    ),
     error: {
       color: errorColor
     },
@@ -45,7 +43,7 @@ const getStyles = (props, context, state) => {
       width: 48,
       height: 48,
       padding: 12,
-      position: 'absolute',
+      position: "absolute",
       top: 0,
       right: 0
     },
@@ -54,25 +52,25 @@ const getStyles = (props, context, state) => {
       width: 24,
       height: 24
     }
-  }
+  };
 
-  return styles
-}
+  return styles;
+};
 
 class PasswordField extends React.Component {
-  constructor (props) {
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       focused: false,
       visible: props.visible
-    }
+    };
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     if (nextProps.visible !== this.props.visible) {
       this.setState({
         visible: nextProps.visible
-      })
+      });
     }
   }
 
@@ -80,27 +78,27 @@ class PasswordField extends React.Component {
    * Toogles the visibility the password.
    * @public
    */
-  toggleVisibility () {
+  toggleVisibility() {
     this.setState({
       visible: !this.state.visible
-    })
+    });
   }
 
-  handleInputFocus (event) {
-    this.setState({ focused: true })
+  handleInputFocus(event) {
+    this.setState({ focused: true });
     if (this.props.onFocus) {
-      this.props.onFocus(event)
+      this.props.onFocus(event);
     }
   }
 
-  handleInputBlur (event) {
-    this.setState({ focused: false })
+  handleInputBlur(event) {
+    this.setState({ focused: false });
     if (this.props.onBlur) {
-      this.props.onBlur(event)
+      this.props.onBlur(event);
     }
   }
 
-  render () {
+  render() {
     const {
       disableButton,
       hintText,
@@ -110,59 +108,59 @@ class PasswordField extends React.Component {
       visibilityButtonStyle,
       visibilityIconStyle,
       style: {
-        width = this.props.fullWidth ? '100%' : 'inherit',
+        width = this.props.fullWidth ? "100%" : "inherit",
         ...otherStyle
       } = {},
       type, // eslint-disable-line
       ...other
-    } = this.props
+    } = this.props;
 
-    const {
-      visible
-    } = this.state
+    const { visible } = this.state;
 
-    const styles = getStyles(this.props, this.context, this.state)
+    const styles = getStyles(this.props, this.context, this.state);
 
-    const { prepareStyles } = this.context.muiTheme
-    const actualErrorText = errorText || hintText
+    const { prepareStyles } = this.context.muiTheme;
+    const actualErrorText = errorText || hintText;
 
     return (
       <div style={{ ...styles.root, ...otherStyle }}>
         <TextField
           {...other}
-          errorStyle={{...styles.error, ...errorStyle}}
+          errorStyle={{ ...styles.error, ...errorStyle }}
           errorText={errorText}
           hintText={null}
           style={{ ...styles.input, width, ...textFieldStyle }}
-          type={visible ? 'text' : 'password'}
-          onFocus={(event) => this.handleInputFocus(event)}
-          onBlur={(event) => this.handleInputBlur(event)}
+          type={visible ? "text" : "password"}
+          onFocus={event => this.handleInputFocus(event)}
+          onBlur={event => this.handleInputBlur(event)}
         />
-        {hintText && !errorText ? <div style={prepareStyles(styles.hint)}>{actualErrorText}</div> : null}
+        {hintText && !errorText
+          ? <div style={prepareStyles(styles.hint)}>{actualErrorText}</div>
+          : null}
         <IconButton
           onTouchTap={() => this.toggleVisibility()}
-          iconStyle={{...styles.visibilityIcon, ...visibilityIconStyle}}
-          style={{...styles.visibilityButton, ...visibilityButtonStyle}}
+          iconStyle={{ ...styles.visibilityIcon, ...visibilityIconStyle }}
+          style={{ ...styles.visibilityButton, ...visibilityButtonStyle }}
           disabled={disableButton || other.disabled}
           tabIndex={-1}
         >
           {visible ? <Visibility /> : <VisibilityOff />}
         </IconButton>
       </div>
-    )
+    );
   }
 }
 
 PasswordField.contextTypes = {
   muiTheme: PropTypes.object.isRequired
-}
+};
 
 PasswordField.defaultProps = {
   disableButton: false,
   visible: false
-}
+};
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   PasswordField.propTypes = {
     ...TextField.propTypes,
     /**
@@ -189,9 +187,9 @@ if (process.env.NODE_ENV !== 'production') {
      * Override the inline-styles of the [SvgIcon](http://www.material-ui.com/v0.18.6/#/components/svg-icon) element used for the visibility icon.
      */
     visibilityIconStyle: PropTypes.object
-  }
-  delete PasswordField.propTypes.multiLine
-  delete PasswordField.propTypes.type
+  };
+  delete PasswordField.propTypes.multiLine;
+  delete PasswordField.propTypes.type;
 }
 
-export default PasswordField
+export default PasswordField;
